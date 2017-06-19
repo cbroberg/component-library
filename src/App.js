c2a4033e69863f0b887fb076d34c3c9dbd94106a
Christian Broberg <cb@webhouse.dk>
2017-06-19 19:30:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import { Button, Button2 } from 'components/components'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button>New button 1</Button>
+        <Button2>New button 2</Button2>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/components.js
@@ -0,0 +1,8 @@
+//Framework components
+import { Button } from 'components/Buttons/Button'
+import { Button2 } from 'components/Buttons/Button2'
+
+export {
+    Button,
+    Button2
+}
